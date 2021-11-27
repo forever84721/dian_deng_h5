b96964aabaa1460e109df0b5ab2fe7358cc263e7
Jay <forever84721@yahoo.com.tw>
2021-11-27 19:38:55 +0800

    deploy

--- src/utils/request/service.js
+++ src/utils/request/service.js
@@ -1,48 +1,40 @@
-import Vue from 'vue'
-import axios from 'axios';
-import store from '@/store';
-import vant from 'vant';
-import router from '@/router'
+import Vue from "vue";
+import axios from "axios";
+import store from "@/store";
+import vant from "vant";
+import router from "@/router";
 
-
-
-Vue.use(vant)
-
-
+Vue.use(vant);
 
 // 创建axios实例
 let service;
 
-
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === "development") {
   service = axios.create({
-    baseURL: '/api' // api的base_url
+    baseURL: "/api" // api的base_url
   });
 } else {
   // 生产环境下
   service = axios.create({
-    baseURL: 'https://wenxuanguangmingdeng.com' // api的base_url
+    // baseURL: 'https://wenxuanguangmingdeng.com' // api的base_url
+    baseURL: "https://platform.freedomstate.idv.tw"
   });
 }
 
 service.defaults.timeout = 50000; // 请求超时时间
 
-
-
-
 // request拦截器 axios的一些配置
 service.interceptors.request.use(
   config => {
     // 判断是否存在token，如果存在的话，则每个http header都加上Access-Token
-    console.log(store.state.user)
+    console.log(store.state.user);
     if (store.state.user.token) {
-      config.headers['Access-Token'] = store.state.user.token;
-
+      config.headers["Access-Token"] = store.state.user.token;
     } else {
-      let user = localStorage.getItem('userMsg');
-      if (user){
+      let user = localStorage.getItem("userMsg");
+      if (user) {
         // console.log(user);
-        config.headers['Access-Token'] = JSON.parse(user).token;
+        config.headers["Access-Token"] = JSON.parse(user).token;
       }
 
       // let result = store.dispatch("GET_USER_EXIST");
@@ -63,35 +55,34 @@ service.interceptors.request.use(
   },
   error => {
     // Do something with request error
-    store.commit('hideLoading')
-
-  },
+    store.commit("hideLoading");
+  }
 );
 
 service.interceptors.response.use(
   response => {
-    store.commit('hideLoading')
+    store.commit("hideLoading");
     if (response.data) {
       switch (response.data.code) {
-         case 403 :
+        case 403:
           // localStorage.removeItem('userMsg');
           // router.replace({path:'/login'});
           break;
-          case 407:
-            // this.$toast('登录失效，请重新登录');
-            router.replace({path:'/login'});
+        case 407:
+          // this.$toast('登录失效，请重新登录');
+          router.replace({ path: "/login" });
           break;
       }
     }
     return response;
   },
   error => {
-    store.commit('hideLoading')
+    store.commit("hideLoading");
     if (error.data) {
       switch (error.data.code) {
         case 407:
           // this.$toast('登录失效，请重新登录');
-          router.replace({path:'/login'});
+          router.replace({ path: "/login" });
           break;
         case 403:
           // localStorage.removeItem('userMsg');
@@ -100,8 +91,6 @@ service.interceptors.response.use(
       }
     }
   }
-
 );
-
 
 export default service;
